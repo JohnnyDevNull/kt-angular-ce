d574512722aeed22b9bd622c17bc3aca77658480
JohnnyDevNull <philipp.john@live.de>
2024-05-20 13:15:39 +0200

    build: add e2e tests

--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -1,13 +1,29 @@
-import { getGreeting } from '../support/app.po';
+import {
+  getApiDataForm,
+  getApiDataJson,
+  getFieldA,
+  getFieldB,
+  getFieldC, getFieldE, getFieldI,
+  getFieldJ, getFieldK, getFieldL,
+  getGreeting
+} from '../support/app.po';
 
 describe('e2e', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    getGreeting().should('contain.text', 'Angular Coding Exercise');
+    getApiDataJson().should('be.visible').and('not.be.empty');
+    getApiDataForm().should('be.visible');
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
+    getFieldA().should('have.class', 'mat-mdc-checkbox-checked');
+    getFieldJ().should('not.have.class', 'mat-mdc-checkbox-checked');
+
+    getFieldB().should('have.value', 44);
+    getFieldC().should('have.value', 'Hello, Klicktipp!');
+    getFieldE().should('have.value', 'I am a nested object with strings!');
+    getFieldI().should('have.value', 'So am I!');
+    getFieldK().should('have.value', 123);
+    getFieldL().should('have.value', 'And I am last!');
   });
 });
